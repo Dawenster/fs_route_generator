fa539984c631026eaf068f050b47cc11dafa8388
Dawenster <dstwen@gmail.com>
2013-12-02 21:39:22 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FsRouteGenerator::Application.config.secret_key_base = 'adabcc306b395d55d8e69e4f3d80c1c9e5645623e5392bb95bb54824e12db30a971d553e9c17576d91bd231b74223490ad624e566e4587dbd8c407adbf9fd0dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FsRouteGenerator::Application.config.session_store :cookie_store, key: '_fs_route_generator_session'
